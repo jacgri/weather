17ae1da8e91c020c6f0c9bb9af2ffe658dc7577e
Jacqui Grimes <grimesjacqui@gmail.com>
2017-07-14 19:15:45 +0100

    Getting weather data from API

---
+++ js/request.js
@@ -0,0 +1,12 @@
+var request = new XMLHttpRequest()
+request.open('GET','http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=b8e159c7fd7ab55ded8ee53d2b65f5b6')
+request.onreadystatechange = function(){
+    if (this.readyState===4 && this.status===200){
+        var responseJSON = JSON.parse(this.response)
+        var temperature = responseJSON.main.temp
+        var weatherDescription = responseJSON.weather[0].description
+
+    }
+
+}
+request.send()
