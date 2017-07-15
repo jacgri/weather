3a7c1d1f3a1d83fe6f6a3275dea8df77792107b4
Jacqui Grimes <grimesjacqui@gmail.com>
2017-07-15 12:07:55 +0100

    adding symbols to the weather

---
+++ js/weather.js
@@ -0,0 +1,11 @@
+var weatherDesc = {
+clearsky:"Clear Sky", //<i class="fa fa-sun-o" aria-hidden="true"></i>
+fewclouds:"Few Clouds", //<i class="fa fa-cloud" aria-hidden="true"></i>
+scatteredclouds:"Scattered Clouds",
+brokenclouds:"Broken Clouds",
+showerrain:"Shower Rain", //<i class="fa fa-tint" aria-hidden="true"></i>
+rain:"Rain",
+thunderstorm:"Thunderstorm",//<i class="fa fa-bolt" aria-hidden="true"></i>
+snow:"Snow",//<i class="fa fa-snowflake-o" aria-hidden="true"></i>
+mist:"Mist",
+}
